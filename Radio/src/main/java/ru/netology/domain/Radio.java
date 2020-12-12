43cfc772f53c7ace4ce5652f7c25dbfa8adf2ef2
Irina Anichkova <anichkova2016@yandex.ru>
2020-12-12 11:41:43 +0300

    Modificated Radio.java and RadioTest.java

--- Radio/src/main/java/ru/netology/domain/Radio.java
+++ Radio/src/main/java/ru/netology/domain/Radio.java
@@ -1,5 +1,7 @@
 package ru.netology.domain;
 public class Radio {
+    private int CurrentVolume;
+    private int CurrentChannel;
     private int maxVolume = 10;
     private int minVolume = 0;
     private int maxChannel = 9;
@@ -7,69 +9,103 @@ public class Radio {
     private int currentVolume;
     private int currentChannel;
 
-
-    public int getCurrentChannel() {
-        return currentChannel;
-    }
-
-    public int getCurrentVolume() {
-        return currentVolume;
-    }
-
-    public void setCurrentVolume(int currentVolume) {
-        if(currentVolume>maxVolume){
+    public void increaseChannel() {
+        if (currentChannel == maxChannel) {
+            this.currentChannel = minChannel;
             return;
         }
-        if(currentVolume<minVolume){
-            return;
-        }
-        this.currentVolume=currentVolume;
-
+        currentChannel++;
     }
 
-    public void setCurrentChannel(int currentChannel) {
-        {
-            if (currentChannel >= maxChannel) {
+    public void decreaseChannel() {
+        if (currentChannel == minChannel) {
+            this.currentChannel = maxChannel;
+            return;
+        }
+        currentChannel--;
+    }
+
+    public void increaseVolume() {
+        if (currentVolume == maxVolume) {
+            return;
+        }
+        currentVolume++;
+    }
+
+    public void decreaseVolume() {
+        if (currentVolume == minVolume) {
+            return;
+        }
+        currentVolume--;
+    }
+
+    public int getCurrentChannel(){
+            return currentChannel;
+        }
+
+        public int getCurrentVolume() {
+            return currentVolume;
+        }
+
+        public void setCurrentVolume(int currentVolume){
+            if (currentVolume > maxVolume) {
+                this.currentVolume = maxVolume;
                 return;
             }
-            if (currentChannel <= minChannel) {
+            if (currentVolume < minVolume) {
+                this.currentVolume = minVolume;
+                return;
+            }
+            this.currentVolume = currentVolume;
+        }
+
+
+        public void setCurrentChannel ( int currentChannel){
+            if (currentChannel > maxChannel) {
+                this.currentChannel = maxChannel;
+                return;
+            }
+            if (currentChannel < minChannel) {
+                this.currentChannel = minChannel;
                 return;
             }
             this.currentChannel = currentChannel;
         }
-        this.currentChannel = currentChannel;
+
+
+        public int getMaxChannel () {
+            return maxChannel;
+        }
+
+        public void setMaxChannel ( int maxChannel){
+            this.maxChannel = maxChannel;
+        }
+
+        public int getMaxVolume () {
+            return maxVolume;
+        }
+
+        public void setMaxVolume ( int maxVolume){
+            this.maxVolume = maxVolume;
+        }
+
+        public int getMinChannel () {
+            return minChannel;
+        }
+
+        public void setMinChannel ( int minChannel){
+            this.minChannel = minChannel;
+        }
+
+        public int getMinVolume () {
+            return minVolume;
+        }
+
+        public void setMinVolume ( int minVolume){
+            this.minVolume = minVolume;
+        }
     }
 
-    public int getMaxChannel() {
-        return maxChannel;
-    }
 
-    public void setMaxChannel(int maxChannel) {
-        this.maxChannel = maxChannel;
-    }
 
-    public int getMaxVolume() {
-        return maxVolume;
-    }
-
-    public void setMaxVolume(int maxVolume) {
-        this.maxVolume = maxVolume;
-    }
-
-    public int getMinChannel() {
-        return minChannel;
-    }
-
-    public void setMinChannel(int minChannel) {
-        this.minChannel = minChannel;
-    }
-
-    public int getMinVolume() {
-        return minVolume;
-    }
-
-    public void setMinVolume(int minVolume) {
-        this.minVolume = minVolume;
-    }
-}
 
